90705625154d270d845db84e9d54c497873480e5
Happysttim <gmlsdyd1023@naver.com>
2025-12-18 14:45:43 +0900

    ✨feat: yCollab 으로 문서 동기화

--- apps/client/src/widgets/code-editor/ui/CodeEditor.tsx
+++ apps/client/src/widgets/code-editor/ui/CodeEditor.tsx
@@ -2,17 +2,25 @@ import { useEffect, useRef } from "react";
 import { EditorView, basicSetup } from "codemirror";
 import { javascript } from "@codemirror/lang-javascript";
 import { githubLight } from "@fsegurai/codemirror-theme-github-light";
+import { useYDoc } from "@/shared/lib/hooks/useYDoc";
+import { yCollab } from 'y-codemirror.next';
 
 export default function CodeEditor() {
   const editorRef = useRef<HTMLDivElement>(null);
   const viewRef = useRef<EditorView | null>(null);
+  const { yText, awareness } = useYDoc('prototype');
 
   useEffect(() => {
     if (!editorRef.current) return;
 
     const view = new EditorView({
-      doc: "// Write your code here\n\nfunction hello() {\n  console.log('Hello, CodeJam!');\n}\n",
-      extensions: [basicSetup, javascript(), githubLight],
+      doc: yText.toString(),
+      extensions: [
+        basicSetup, 
+        javascript(),
+        yCollab(yText, awareness), 
+        githubLight
+      ],
       parent: editorRef.current,
     });
 
