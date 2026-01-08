a14cefbd048d652c6c8020793cd4ae4bc2e56b7a
son-hyejun <h_jun0515@naver.com>
2026-01-08 14:59:54 +0900

    ✨ feat: 만료된 Room 정리 스케줄러 추가 #76
    - ScheduleModule을 CleanupModule에 등록
    - RoomModule, CollaborationModule 의존성 추가
    - handleRoomCleanup 메서드에 @Cron 데코레이터 적용 (매시간 실행)
    - RoomService, CollaborationGateway 주입
    - 만료된 방 정리 로직 TODO 주석 추가
    - Logger 추가로 스케줄 실행 로그 기록

--- apps/server/src/modules/cleanup/cleanup.module.ts
+++ apps/server/src/modules/cleanup/cleanup.module.ts
@@ -1,7 +1,11 @@
 import { Module } from '@nestjs/common';
 import { CleanupService } from './cleanup.service';
+import { ScheduleModule } from '@nestjs/schedule';
+import { RoomModule } from '../room/room.module';
+import { CollaborationModule } from '../collaboration/collaboration.module';
 
 @Module({
-  providers: [CleanupService]
+  imports: [ScheduleModule.forRoot(), RoomModule, CollaborationModule],
+  providers: [CleanupService],
 })
 export class CleanupModule {}
--- apps/server/src/modules/cleanup/cleanup.service.ts
+++ apps/server/src/modules/cleanup/cleanup.service.ts
@@ -1,4 +1,26 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
+import { RoomService } from '../room/room.service';
+import { CollaborationGateway } from '../collaboration/collaboration.gateway';
+import { Cron, CronExpression } from '@nestjs/schedule';
 
 @Injectable()
-export class CleanupService {}
+export class CleanupService {
+  private readonly logger = new Logger(CleanupService.name);
+
+  constructor(
+    private readonly roomService: RoomService,
+    private readonly collaborationGateway: CollaborationGateway,
+  ) {}
+
+  @Cron(CronExpression.EVERY_HOUR)
+  async handleRoomCleanup() {
+    this.logger.log('🕒 Starting scheduled room cleanup...');
+
+    /**
+     * TODO:
+     * 1. 만료된 방 조회
+     * 2. 소켓 알림 및 연결 종료 (방 별로 순회)
+     * 3. DB에서 방 삭제
+     */
+  }
+}
