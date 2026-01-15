9849f7aa061a36f6ac75044708bc6ff8e83fe28b
Happysttim <gmlsdyd1023@naver.com>
2026-01-16 00:33:32 +0900

    ✨feat: editor 권한 이상 시 파일 우클릭 시 context 오픈

--- apps/client/src/widgets/files/index.tsx
+++ apps/client/src/widgets/files/index.tsx
@@ -1,11 +1,17 @@
 import { useFileStore } from '@/stores/file';
 import { File } from './File';
+import { useRoomStore } from '@/stores/room';
+import { usePt } from '@/stores/pts';
 
 export function FileList() {
   const getFileIdMap = useFileStore((state) => state.getFileIdMap);
   const fileMap = getFileIdMap();
   const count = fileMap?.size ?? 0;
   const entries: [string, string][] = Object.entries(fileMap?.toJSON() ?? {});
+
+  const myPtId = useRoomStore((state) => state.myPtId);
+  const me = usePt(myPtId);
+  const hasPermission = me?.role === 'host' || me?.role === 'editor';
 
   return (
     <div className="w-full min-w-3xs bg-white dark:bg-gray-800 p-4 font-sans">
@@ -15,7 +21,12 @@ export function FileList() {
 
       <div className="space-y-1 mt-4">
         {entries.map(([fileName, fileId]) => (
-          <File key={fileId} fileId={fileId} fileName={fileName} />
+          <File
+            key={fileId}
+            fileId={fileId}
+            fileName={fileName}
+            hasPermission={hasPermission}
+          />
         ))}
       </div>
     </div>
