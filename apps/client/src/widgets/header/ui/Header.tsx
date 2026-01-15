46605a3001f131082e518888bc37d0418a8f9df6
Happysttim <gmlsdyd1023@naver.com>
2026-01-15 11:22:37 +0900

    ✨feat: 새 파일 및 파일 업로드 기능 작성

--- apps/client/src/widgets/header/ui/Header.tsx
+++ apps/client/src/widgets/header/ui/Header.tsx
@@ -1,6 +1,5 @@
 import { PROJECT_NAME } from "@codejam/common";
-import { useState } from "react";
-import { useRoomStore } from "@/stores/room";
+import { useRef, useState, type ChangeEvent } from "react";
 import LogoAnimation from "@/assets/logo_animation.svg";
 import { Button } from "@/shared/ui/button";
 import { Input } from "@/shared/ui/input";
@@ -24,13 +23,28 @@ import {
   Settings,
   Sun,
   Moon,
+  Plus,
 } from "lucide-react";
+import { toast } from "sonner";
+import { useFileRename } from "@/shared/lib/hooks/useFileRename";
+import { extname } from "@/shared/lib/file";
+import { NewFileDialog } from "@/widgets/dialog/NewFileDialog";
+import { useFileStore } from "@/stores/file";
+import { DuplicateDialog } from "@/widgets/dialog/DuplicateDialog";
 
-export default function Header() {
-  const roomCode = useRoomStore((state) => state.roomCode);
+type HeaderProps = {
+  roomCode: string;
+};
 
+export default function Header({ roomCode }: HeaderProps) {
+  const { setIsDuplicated, isDuplicated, handleCheckRename } = useFileRename(roomCode);
   const [isDark, setIsDark] = useState(false);
   const [isCopied, setIsCopied] = useState(false);
+  const { getFileId, createFile } = useFileStore();
+  const [uploadFile, setUploadFile] = useState<File | undefined>(undefined);
+  const [filename, setFilename] = useState("");
+
+  const uploadRef = useRef<HTMLInputElement>(null);
 
   const toggleDarkMode = () => {
     setIsDark(!isDark);
@@ -50,6 +64,89 @@ export default function Header() {
 
   const handleNotImplemented = (feature: string) => {
     alert(`${feature} 기능은 아직 구현되지 않았습니다.`);
+  };
+
+  const handleUploadButton = () => {
+    if (!uploadRef.current) {
+      toast.error("오류가 발생했습니다. 새로고침을 해주세요.");
+      return;
+    }
+
+    uploadRef.current.click();
+  };
+
+  const handleFileChange = async (ev: ChangeEvent<HTMLInputElement>) => {
+    const currentMimes = [
+      "text/javascript",
+      "text/html",
+      "text/css",
+    ];
+    const currentExts = [
+      "ts",
+      "tsx",
+      "jsx"
+    ];
+    const MAX_SIZE = 1024 * 1024;
+
+    const files = ev.target.files;
+
+    if (!roomCode) {
+      toast.error("유효하지 않는 방 코드 입니다.");
+      return;
+    }
+
+    if (!files || files.length === 0) {
+      toast.error("파일을 하나 이상 선택해주세요.");
+      return;
+    }
+
+    const uploadFile = files[0];
+
+    if (!currentMimes.includes(uploadFile.type) && !currentExts.includes(extname(uploadFile.name))) {
+      toast.error("정해진 파일 타입만 업로드 할 수 있습니다.");
+      return;
+    }
+
+    if (uploadFile.size > MAX_SIZE) {
+      toast.error("파일의 크기가 1MB 를 초과했습니다.");
+      return;
+    }
+
+    if (uploadRef.current) {
+      uploadRef.current.value = "";
+    }
+
+    setUploadFile(uploadFile);
+
+    if (getFileId(uploadFile.name)) {
+      setFilename(uploadFile.name);
+      setIsDuplicated(true);
+    } else {
+      const result = await handleCheckRename(uploadFile.name);
+      if (result) {
+        const content = await uploadFile.text();
+        createFile(uploadFile.name, content);
+        setUploadFile(undefined);
+      }
+    }
+  };
+
+  const handleNewFile = async (name: string, ext: string) => {
+    const filename = `${name}.${ext}`;
+    if (getFileId(filename)) {
+      setFilename(filename);
+      setIsDuplicated(true);
+    } else {
+      const result = await handleCheckRename(filename);
+      if (result) {
+        createFile(filename, "");
+      }
+    }
+  }
+
+  const handleDuplicateDialog = () => {
+    setUploadFile(undefined);
+    setFilename("");
   };
 
   return (
@@ -86,14 +183,29 @@ export default function Header() {
         </div>
       </div>
 
+      {/* 파일 중복 다이어로그 */}
+      <DuplicateDialog 
+        open={isDuplicated}
+        onOpenChange={setIsDuplicated}
+        filename={filename}
+        file={uploadFile}
+        onClick={handleDuplicateDialog} />
+
       {/* 우측 액션 버튼들 */}
       <div className="ml-auto flex items-center gap-1">
+        <NewFileDialog onSubmit={handleNewFile}>
+          <Button variant="ghost" size="sm" className="gap-1.5 text-xs h-8">
+            <Plus className="h-4 w-4" />
+            <span>New File</span>
+          </Button>
+        </NewFileDialog>
         <Button
           variant="ghost"
           size="sm"
           className="gap-1.5 text-xs h-8"
-          onClick={() => handleNotImplemented("Upload")}
+          onClick={handleUploadButton}
         >
+          <input type="file" ref={uploadRef} className="hidden" accept="text/javascript,text/css,text/html,.ts,.tsx,.jsx" onChange={handleFileChange} />
           <Upload className="h-4 w-4" />
           <span>Upload</span>
         </Button>
