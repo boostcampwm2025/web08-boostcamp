27fa2cdba36f7fb3477c7ac2afd557f6f9295a0a
son-hyejun <h_jun0515@naver.com>
2026-01-27 23:53:45 +0900

    🐛 fix: cookie 관련 프록시 trust, same site 설정 변경
    - Caddy를 신뢰하도록 설정
    - 배포 환경일 때 same site false로 설정

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -4,9 +4,10 @@ import { ConfigService } from '@nestjs/config';
 import { PROJECT_NAME } from '@codejam/common';
 import { ZodValidationPipe } from 'nestjs-zod';
 import { HttpExceptionFilter } from './common/filters/http-exception.filter';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
   const configService = app.get(ConfigService);
   const port = configService.get<number>('PORT') ?? 3000;
 
@@ -15,6 +16,9 @@ async function bootstrap() {
 
   // 전역 예외 필터 등록
   app.useGlobalFilters(new HttpExceptionFilter());
+
+  // Caddy(Reverse Proxy)를 신뢰하도록 설정
+  app.set('trust proxy', 1);
 
   app.enableCors({
     origin: true,
