34d786ea1dcc7d651e3b51674457db81ff61f0bf
Sanghwa Song <cnrrn421@gmail.com>
2026-01-27 22:34:29 +0900

    🐛fix: 타입 체크 수정

--- apps/server/src/modules/code-execution/code-execution.service.spec.ts
+++ apps/server/src/modules/code-execution/code-execution.service.spec.ts
@@ -19,6 +19,7 @@ const mockHttpService = {
 
 describe('CodeExecutionService', () => {
   let service: CodeExecutionService;
+  let httpService: HttpService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
