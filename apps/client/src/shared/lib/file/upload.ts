bc43fb28868df658055b76839b1416efbc9c2a03
Sanghwa Song <cnrrn421@gmail.com>
2026-02-02 00:10:54 +0900

    ✨feat: upload 시 text 또는 image 구분 추가

--- apps/client/src/shared/lib/file/upload.ts
+++ apps/client/src/shared/lib/file/upload.ts
@@ -5,9 +5,30 @@ interface UploadFileResult {
   type: FileType;
 }
 
+export function getType(file: File): string {
+  if (!file.type) return 'unknown';
+  const type = file.type.split('/')[0];
+  console.log(file.type);
+  return type;
+}
+
 export async function uploadFile(file: File): Promise<UploadFileResult> {
-  const content = await uploadTextFile(file);
-  return { content, type: 'text' };
+  const type = getType(file);
+
+  switch (type) {
+    case 'text': {
+      const content = await uploadTextFile(file);
+      return { content, type: 'text' };
+    }
+    case 'image': {
+      const url = await uploadImageFile(file);
+      return { content: url, type: 'image' };
+    }
+    default: {
+      const content = await uploadTextFile(file);
+      return { content, type: 'text' };
+    }
+  }
 }
 
 /**
@@ -15,5 +36,14 @@ export async function uploadFile(file: File): Promise<UploadFileResult> {
  * @param file - The file to read
  */
 export async function uploadTextFile(file: File): Promise<string> {
-  return await file.text();
+  const content = await file.text();
+  return content;
+}
+
+/**
+ * Reads the content of a file and returns it as a URL string
+ */
+export async function uploadImageFile(file: File): Promise<string> {
+  const url = await file.text();
+  return url;
 }
